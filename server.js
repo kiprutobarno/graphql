c2e372ed19545e446647cebdc5c5d7a0144ac950
ywalakamar <barxwells@gmail.com>
2020-01-02 13:04:14 +0300

    feature(object types): object types - closes #9

--- server.js
+++ server.js
@@ -3,25 +3,48 @@ const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
     type Query {
-        rollDice(numDice: Int!, numSides: Int):[Int]
+        getDie(numSides: Int): RandomDie
     }
 `);
 
-const root = {
-  rollDice: ({ numDice, numSides }) => {
-    const output = [];
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+//This class implements the RandomDie GraphQL type
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({ numRolls }) {
+    let output = [];
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
   }
+}
+
+const root = {
+  getDie: ({ numSides }) => {
+    return new RandomDie(numSides || 6);
+  }
 };
 
-/** Query */
 // {
-//   rollDice((numDice: 2), (numSides: 6));
-// }
+//     getDie(numSides: 6) {
+//       rollOnce
+//       roll(numRolls: 3)
+//     }
+//   }
 
 const app = express();
 app.use(

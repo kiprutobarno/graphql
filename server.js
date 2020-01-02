eb71d3aa3743a8eea684a5f9f72cefacf50977f5
ywalakamar <barxwells@gmail.com>
2020-01-02 11:43:57 +0300

    chore(server):express GraphQL server - closes #3 - running an express GraphQL server

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const { graphql, buildSchema} = require("graphql");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-/**construct a schema using GraphQL schema language */
 const schema = buildSchema(`type Query {hello:String}`);
 
-/**The root provides a resolver function for each API endpoint */
 const root = {
   hello: () => {
-    return "Hello world";
+    return `Hello world!`;
   }
 };
 
-/**Run the graphql query {hello} and print the response*/
-graphql(schema, "{hello}", root).then(response => {
-  console.log(response);
-});
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
